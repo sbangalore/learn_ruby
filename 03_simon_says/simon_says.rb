2f801042ce3e44d2adc3d455340c3cf3032ee72e
Sudarshan <bsudarshan@outlook.com>
2016-08-25 11:06:36 -0400

    finished pig latin translator

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(word)
+    return word
+end
+
+def shout(word)
+    return echo(word).upcase
+end
+
+def repeat(word, times=2)
+    return word + (' '+word) * (times-1)
+end
+
+def start_of_word(word, chars)
+    return word[0, chars]
+end
+
+def first_word(word)
+    return word.split.first()
+end
+
+def titleize(word)
+    words = word.split(" ")
+    initial = word.split[0]
+    result = initial[0].upcase + initial[1,initial.length]
+    words.drop(1).each do |value|
+        result << " " + value[0].upcase + value[1,value.length]
+    end
+    return result
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
 #write your code here
+#pig latin
+def translate str
+    words = str.split(' ')
+    result = []
+    alpha = ('a' .. 'z').to_a
+    vowels = ['a','e','i','o','u']
+    consonants = alpha - vowels
+    
+    words.each do |word|
+        if vowels.include?(word[0])
+            result.push(word + 'ay')
+        elsif consonants.include?(word[0]) && 'q'.include?(word[1]) && 'u'.include?(word[2])
+            result.push(word[3..-1] + word[0..2] + 'ay')
+        elsif 'q'.include?(word[0]) && 'u'.include?(word[1])
+            result.push(word[2..-1] + word[0..1] + 'ay')
+        elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+            result.push(word[3..-1] + word[0..2] + 'ay')
+        elsif consonants.include?(word[0]) && consonants.include?(word[1])
+            result.push(word[2..-1] + word[0..1] + 'ay')
+        elsif consonants.include?(word[0])
+            result.push(word[1..-1] + word[0] + 'ay')
+        else
+            result.push(word)
+        end
+    end
+
+ result.join(' ')
+
+end
