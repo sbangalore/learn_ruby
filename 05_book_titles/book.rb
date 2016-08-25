55f9a3b45f1fd40c1fe6bbbc03074261e5567dba
Sudarshan <bsudarshan@outlook.com>
2016-08-25 16:49:41 -0400

    updated Booktitles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+
+    attr_accessor :title
+    
+    def title=(new_title)
+        words = new_title.split(' ')
+        prepositions = ['on','in','at', 'since', 'for', 'ago', 'before', 'to', 'past', 'to', 'till','until','by']
+        little_words = ['and','the','or','over','the','a','an','but','for','and','nor','yet','so','of'] + prepositions
+        result = [words[0][0].upcase+words[0][1..-1]]
+        words[1..-1].each do |word|
+            if little_words.include? word
+                result.push(word)
+            else
+                word = word[0].upcase + word[1..-1]
+                result.push(word)
+            end
+        end
+        @title = result.join(' ')
+    end
 end
